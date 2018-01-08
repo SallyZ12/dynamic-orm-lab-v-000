c3c97e95c78f739cb61676360ff1528418a20eef
Sally Zasloff <szasloff@gmail.com>
2018-01-08 23:39:45 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -63,7 +63,7 @@ class InteractiveRecord
 
     key = attribute.keys.first
     value = attribute.values.first
-    formatted_value = value.class == Fixnum ? value: "'#{value}'"
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
     binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE #{key} = #{formatted_value}"
     DB[:conn].execute(sql)
